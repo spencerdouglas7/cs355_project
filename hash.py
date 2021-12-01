83d51f09984e7b56041476076238e573487bf053
spencerdouglas7 <dougla55@purdue.edu>
2021-11-30 19:50:49 -0500

    rudimentary prgs and hashing

---
+++ hash.py
@@ -0,0 +1,22 @@
+from Crypto.Hash import SHA256
+
+# @author Spencer Douglas
+# Given a @param string, @return the SHA-256 hash of the respective string, or None if a string is not provided.
+def SHA_hash(string):
+    if string is None or type(string) is not str:
+        return
+    h = SHA256.new()
+    #convert to bytes for hash
+    bytedata = bytes(string, 'utf-8')
+    h.update(bytedata)
+    return h.hexdigest()
+
+def SHA_hash_test():
+    print(SHA_hash('Hello'))
+    print(SHA_hash('Hello'))
+    print(SHA_hash(None))
+    print(SHA_hash({}))
+    print(SHA_hash('Zoo Wee Mama'))
+
+
+SHA_hash_test()
---
+++ prg.py
@@ -0,0 +1,20 @@
+import secrets
+
+PRG_LENGTH = 256
+
+def crypto_random():
+    #g = _UserFriendlyRNG.new()
+    return secrets.randbits(PRG_LENGTH)
+    #return g.get_random_bytes(PRG_LENGTH)
+
+
+def crypto_random_test():
+    map = {}
+    for i in range(100):
+        val = crypto_random()
+        if map.get(val):
+            return False
+        map.update({val: val})
+    return True
+
+print(crypto_random_test())
